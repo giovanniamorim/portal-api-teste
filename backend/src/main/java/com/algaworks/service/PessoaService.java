405efbe16205d1825aac049c25f6d7f1c52ac253
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-21 14:54:01 -0300

    Atualizando Pessoa com PUT

---
+++ backend/src/main/java/com/algaworks/service/PessoaService.java
@@ -0,0 +1,26 @@
+package com.algaworks.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.model.Pessoa;
+import com.algaworks.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = this.pessoaRepository.findById(codigo)
+				.orElseThrow(() -> new EmptyResultDataAccessException(1));
+		
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		
+		return this.pessoaRepository.save(pessoaSalva);
+
+	}
+}
