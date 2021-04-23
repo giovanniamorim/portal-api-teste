24d7d0fbb2e680e659a65f0a3315bc3f728978eb
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-22 21:09:47 -0300

    Desafio: Removendo Lançamentos

--- backend/src/main/java/com/algaworks/resource/LancamentoResource.java
+++ backend/src/main/java/com/algaworks/resource/LancamentoResource.java
@@ -12,12 +12,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.event.RecursoCriadoEvent;
@@ -49,7 +51,7 @@ public class LancamentoResource {
 		return lancamentoRepository.filtrar(lancamentoFilter);
 	}
 	
-	@GetMapping(value = "/{codigo}")
+	@GetMapping("/{codigo}")
 	public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
 		return this.lancamentoRepository.findById(codigo)
 				.map(lancamento -> ResponseEntity.ok(lancamento))
@@ -63,6 +65,12 @@ public class LancamentoResource {
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		this.lancamentoRepository.deleteById(codigo);
 	}
 	
 	@ExceptionHandler(PessoaInexistenteOuInativaException.class)
